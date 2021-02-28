2d6ae51a6747873d4cbdd18de17d65e670295e17
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2021-02-28 21:47:13 +0300

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,22 +1,57 @@
-#list_nums = [1, 2, 3, 4, 5]
-#print(list_nums[3] == list_nums[-2])
-#print(list_nums)
+import pygame as pg
+from random import randint
 
+class Circle:
+    def __init__(self, x, y, rad, col=(0, 0, 0)):
+        self.x = x
+        self.y = y
+        self.rad = rad
+        self.col = col
 
-import pygame
+    def movement(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_UP]:
+            self.y -= 1
+        if keys[pg.K_DOWN]:
+            self.y += 1
+        if keys[pg.K_LEFT]:
+            self.x -= 1
+        if keys[pg.K_RIGHT]:
+            self.x += 1
 
-pygame.init()
-W = 600
-H = 400
-win = pygame.display.set_mode(W, H)
+    def draw(self, win):
+        pg.draw.circle(win, self.col, (self.x, self.y), self.rad)
+
+    def horizontal_movement(self):
+        self.x += 1
+        if self.x > W:
+            self.x = 0
+
+W = 500
+H = 500
+FPS = 60
+
+list_circles = []
+for i in range(100):
+    circle = Circle(i * 10, i * 5, 50, (randint(0, 255), randint(0, 255), randint(0, 255)))
+    list_circles.append(circle)
+
+pg.init()
+win = pg.display.set_mode((W, H))
+clock = pg.time.Clock()
+
 
 while True:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             exit()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]
-        y -= 1
-    if keys[pygame.K_DOWN]
-        y += 1 # y = y + 1
+    for i in range(100):
+        list_circles[i].horizontal_movement()
+
+    win.fill((255, 255, 255))
+    for i in range(100):
+        list_circles[i].draw(win)
+    pg.display.update()
+    clock.tick(FPS)
